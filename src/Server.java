18e008277f8b0b618e02166116a44681ea3c872a
Jason <jason131>
2019-04-06 18:24:21 -0700

    add classes

---
+++ src/Server.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private static String[] names = {"Willy", "Bob", "Billy", "Joe"};
+    private static String[] adjs = {"the gentle", "the rough", "the overweight", "the urban"};
+    private static final int PORT = 9090;
+
+    private static ArrayList<ClientHandler> clients = new ArrayList<>();
+    private static ExecutorService pool = Executors.newFixedThreadPool(4);
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket listener = new ServerSocket(PORT);
+
+        while (true) {
+            System.out.println("[SERVER] Waiting for client connection... ");
+            Socket client = listener.accept();
+            System.out.println("[SERVER] Connected to client");
+
+            ClientHandler clientThread = new ClientHandler(client, clients);
+            clients.add(clientThread);
+
+            pool.execute(clientThread);
+        }
+    }
+
+
+    public static String getRandomName() {
+        String name = names[(int) (Math.random() * names.length)];
+        String adj = adjs[(int) (Math.random() * adjs.length)];
+        return name + " " + adj;
+    }
+
+}
